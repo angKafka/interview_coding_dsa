4635e4956ec65220a683fb9002721d20e708a480
mean-stack-app <ngtrainerdeveloper@gmail.com>
2024-09-12 16:31:31 +0530

    bubble sort first problem - Sort Colors removed main method

--- src/concept/bubble_sort/array/SortColors.java
+++ src/concept/bubble_sort/array/SortColors.java
@@ -20,13 +20,4 @@ public class SortColors {
         nums[a] = nums[b];
         nums[b] = tempStore;
     }
-
-    public static void main(String[] args) {
-        int[] nums = {2,0,2,1,1,0};
-        SortColors sortColors = new SortColors();
-        sortColors.sortColors(nums);
-        for (int i = 0; i < nums.length; i++) {
-            System.out.print(nums[i]+" ");
-        }
-    }
 }
